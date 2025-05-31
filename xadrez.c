698fc1ce8a25b180a184a28e453e048545f91b4c
Endrewfernando <147087533+sugetudo@users.noreply.github.com>
2025-05-31 00:30:11 +0000

    a

--- xadrez.c
+++ xadrez.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 void space(){
-    printf("-----------------------------");
+    printf("-----------------------------\n");
 }
 void bispo(int n,char* vertical,char* horizontal){
     if(n==0){
-        printf("Movimento Finalizado");
+        printf("Movimento Finalizado\n");
     }else{
             for(int i=0;i<1;i++){
                 printf("%s, ",vertical);
@@ -16,7 +16,22 @@ void bispo(int n,char* vertical,char* horizontal){
         bispo(n-1,vertical,horizontal);
     }
 }
-
+void rainha(int n,char* direcao){
+    if(n==0){
+        printf("movimento finalizado\n");
+    }else{
+        printf("%s\n",direcao);
+        rainha(n-1,direcao);
+    }
+}
+void torre(int n,char* direcao){
+    if(n==0){
+        printf("movimento finalizado\n");
+    }else{
+        printf("%s\n",direcao);
+        rainha(n-1,direcao);
+    }
+}
 int main(){
     int torre_casas=5,bispo_casa=5,rainha_casa=8;
     char* direcao_torre="direita";
@@ -26,5 +41,13 @@ int main(){
     space();
     printf("Bispo se move para %s e %s em %d casas:\n",direcao_bispo_vertical,direcao_bispo_horizontal,bispo_casa);
     bispo(bispo_casa,direcao_bispo_vertical,direcao_bispo_horizontal);
+    space();
+    printf("Rainha se moveu para %s em %d casas:\n",direcao_rainha,rainha_casa);
+    rainha(rainha_casa,direcao_rainha);
+    space();
+    printf("Torre se moveu para %s em %d casas:\n",direcao_torre,torre_casas);
+    torre(torre_casas,direcao_torre);
+
+
     return 0;
 }
